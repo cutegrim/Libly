c0fd75f152dec2d7403673ab5e943e57bab65f79
cutegrim <48733615+cutegrim@users.noreply.github.com>
2022-01-19 21:00:06 +0100

    Add files via upload

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Libly.Dtos;
+using Libly.Models;
+
+namespace Libly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<Book, BookDto>();
+            Mapper.CreateMap<BookDto, Book>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<Membership, MembershipDto>();
+            Mapper.CreateMap<Genre, GenreDto>();
+        }
+    }
+}
